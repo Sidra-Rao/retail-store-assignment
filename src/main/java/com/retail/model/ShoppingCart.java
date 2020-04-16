708a7667e43f3168569f14a115cb00aba983daca
Sidra <engsid.rao@gmail.com>
2020-04-16 09:55:59 +0500

    Update ShoppingCart.java

--- src/main/java/com/retail/model/ShoppingCart.java
+++ src/main/java/com/retail/model/ShoppingCart.java
@@ -37,41 +37,7 @@ public class ShoppingCart extends BaseModel {
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((productId == null) ? 0 : productId.hashCode());
-		result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
-		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ShoppingCart other = (ShoppingCart) obj;
-		if (productId == null) {
-			if (other.productId != null)
-				return false;
-		} else if (!productId.equals(other.productId))
-			return false;
-		if (quantity == null) {
-			if (other.quantity != null)
-				return false;
-		} else if (!quantity.equals(other.quantity))
-			return false;
-		if (userId == null) {
-			if (other.userId != null)
-				return false;
-		} else if (!userId.equals(other.userId))
-			return false;
-		return true;
-	}
+	
 	@Override
 	public String toString() {
 		return "ShoppingCart [productId=" + productId + ", userId=" + userId + ", quantity=" + quantity + "]";
