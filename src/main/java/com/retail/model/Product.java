4fb69bd573d0156715e21ea4ecda31e8cec194bf
Sidra <engsid.rao@gmail.com>
2020-04-16 09:55:08 +0500

    Update Product.java

--- src/main/java/com/retail/model/Product.java
+++ src/main/java/com/retail/model/Product.java
@@ -37,41 +37,7 @@ public class Product extends BaseModel {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((price == null) ? 0 : price.hashCode());
-		result = prime * result + ((productName == null) ? 0 : productName.hashCode());
-		result = prime * result + ((productTypeId == null) ? 0 : productTypeId.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Product other = (Product) obj;
-		if (price == null) {
-			if (other.price != null)
-				return false;
-		} else if (!price.equals(other.price))
-			return false;
-		if (productName == null) {
-			if (other.productName != null)
-				return false;
-		} else if (!productName.equals(other.productName))
-			return false;
-		if (productTypeId == null) {
-			if (other.productTypeId != null)
-				return false;
-		} else if (!productTypeId.equals(other.productTypeId))
-			return false;
-		return true;
-	}
+	
 	@Override
 	public String toString() {
 		return "Product [productTypeId=" + productTypeId + ", productName=" + productName + ", price=" + price + "]";
