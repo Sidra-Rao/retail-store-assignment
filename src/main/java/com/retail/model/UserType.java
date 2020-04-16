721842ebab1a31e4b05dddfb08d688e694259717
Sidra <engsid.rao@gmail.com>
2020-04-16 09:57:01 +0500

    Update UserType.java

--- src/main/java/com/retail/model/UserType.java
+++ src/main/java/com/retail/model/UserType.java
@@ -21,32 +21,7 @@ public class UserType extends BaseModel {
 	public void setUserType(String userType) {
 		this.userType = userType;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((userType == null) ? 0 : userType.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		UserType other = (UserType) obj;
-		if (userType == null) {
-			if (other.userType != null)
-				return false;
-		} else if (!userType.equals(other.userType))
-			return false;
-		return true;
-	}
-
+	
 	@Override
 	public String toString() {
 		return "UserType [userType=" + userType + "]";
